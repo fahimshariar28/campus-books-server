5f589d12e951388877e83d3dc289a7b4e61c885e
Fahim Shariar <fahimshariar28@gmail.com>
2023-07-22 12:11:10 +0600

    User add in the database when creating new account

--- index.js
+++ index.js
@@ -10,6 +10,27 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  // bearer token
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 // MongoDB Connection
 
 const uri = `mongodb+srv://${process.env.DB_ID}:${process.env.DB_PASSWORD}@cluster0.uzgcvif.mongodb.net/?retryWrites=true&w=majority`;
@@ -33,6 +54,29 @@ async function run() {
     const userCollection = client
       .db("campus-books-collection")
       .collection("users");
+
+    // Jwt Token
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+
+      res.send({ token });
+    });
+
+    // Add User to Database
+    app.post("/adduser", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        res.send({ error: true, message: "User already exists" });
+      } else {
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+      }
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
